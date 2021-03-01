0642def2a8f4455a867085792f791992117ddf1b
leecho87 <leecho87@gmail.com>
2021-03-01 23:07:54 +0900

    카카오지도 로드

--- src/pages/location/index.tsx
+++ src/pages/location/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Map } from '@components/location';
+
+declare global {
+    interface Window {
+        kakao: any;
+    }
+}
 
 const Location:React.FC = () => {
+    const { kakao } = window;
+
+    const drawMaps = () => {
+        const container = document.getElementById('TriperMaps');
+		const options = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+		const map = new kakao.maps.Map(container, options);
+    }
+
+    useEffect(() => {
+        drawMaps();
+    }, []);
+
     return (
         <div>
-            location
+            <Map id="TriperMaps" />
         </div>
     )
 }
